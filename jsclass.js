9afa4bad1c1060158f43fb5abda78d97de95bfa5
kkb0411 <yunseok4886@gmail.com>
2025-04-15 21:44:00 +0900

    0415-18

--- jsclass.js
+++ jsclass.js
@@ -1,27 +1,34 @@
-//class h{
-//    let height = 0;
+//class h {
+//    height = 0;
 //
-//    if(height>=150){
-//        console.log("YES");
-//    }
-//    else{
-//        console.log("NO");
+//    check() {
+//        this.height = prompt("입력하세요");
+//        if ( Number(this.height) >= 150 ) {
+//            console.log("YES");
+//        } else {
+//            console.log("NO");
+//        }
 //    }
 //}
+//
+//let l = new h();
+//l.check();
 
-
-class h {
-    height = 0;
-
-    check() {
-        this.height = prompt("입력하세요");
-        if ( Number(this.height) >= 150 ) {
-            console.log("YES");
-        } else {
-            console.log("NO");
-        }
+class average{
+    lan = 0;
+    Math = 0;
+    English = 0;
+    avg = 0;
+    check(){
+        this.average = prompt("입력하세요");
+        Number(this.lan);
+        Number(this.Math);
+        Number(this.English);
+        avg=Math.floor(lan+Math+English)/3;
+        console.log(avg);
     }
 }
+let s = new average();
+s.check();
 
-let l = new h();
-l.check();
+
